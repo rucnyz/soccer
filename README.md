d7af3ec6eeedda34c7589eb673dcaf2a7b0951d9
nieyuzhou <rucnyz@gmail.com>
2022-03-31 16:44:21 +0800

    数据集使用

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## 数据集准备
 
-将所有csv文件放在`./data/`目录下，包括
+从[European Soccer Database](https://www.kaggle.com/datasets/hugomathien/soccer) 下载`database.sqlite`置于`./data/`
+目录下，之后使用`extract_sql.py`转为csv。
+
+或者直接将所有csv文件放在`./data/`
+目录下，包括
 
 - country.csv
 - league.csv
--- extract_sql.py
+++ extract_sql.py
@@ -3,15 +3,27 @@
 # @Author  : nieyuzhou
 # @File    : extract_sql.py
 # @Software: PyCharm
+import os
 import sqlite3
 import pandas as pd
 
-path = "./data/"  # Insert path here
+path = "./data/"
 database = path + 'database.sqlite'
 conn = sqlite3.connect(database)
 
-# Fetching required data tables
+# 获取数据
 player_data = pd.read_sql("SELECT * FROM Player;", conn)
 player_stats_data = pd.read_sql("SELECT * FROM Player_Attributes;", conn)
 team_data = pd.read_sql("SELECT * FROM Team;", conn)
+team_stats_data = pd.read_sql("SELECT * FROM Team_Attributes;", conn)
 match_data = pd.read_sql("SELECT * FROM Match;", conn)
+league_data = pd.read_sql("SELECT * FROM League;", conn)
+country_data = pd.read_sql("SELECT * FROM Country;", conn)
+
+player_data.to_csv(os.path.join(path, "player.csv"), index_col = 0)
+player_stats_data.to_csv(os.path.join(path, "player_attr.csv"), index_col = 0)
+team_data.to_csv(os.path.join(path, "team.csv"), index_col = 0)
+team_stats_data.to_csv(os.path.join(path, "team_attr.csv"), index_col = 0)
+match_data.to_csv(os.path.join(path, "match.csv"), index_col = 0)
+league_data.to_csv(os.path.join(path, "league.csv"), index_col = 0)
+country_data.to_csv(os.path.join(path, "country.csv"), index_col = 0)
